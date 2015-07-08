3200a85a39c4fc750c84c97058f61e6a6b344f1c
Joe Paggi <jpaggi@mit.edu>
2015-07-08 15:19:48 -0400

    added radix sort template fxn

--- gbwt_graph.h
+++ gbwt_graph.h
@@ -1284,15 +1284,9 @@ public:
         };
     };
 
-    struct PathNodeSlice {
-    	PathNode*     place;
-    	PathNode*     end;
-    	bool          just_made;
-
-    	bool operator< (const PathNodeSlice& o) const {
-    		return *place < *(o.place);
-    	}
-    };
+    static index_t PathNodeFrom (PathNode* a) {
+    		return a->from;
+    }
 
     struct PathNodeFromCmp {
         bool operator() (const PathNode& a, const PathNode& b) const {
@@ -1642,6 +1636,86 @@ public: EList<pair<index_t, index_t> > ftab;
 #endif
 };
 
+template <typename T, typename index_t>
+void bin_sort(T* begin, T* end, index_t (*hash)(T*),  int log_size) {
+	const int SHIFT = 7;
+	const int BLOCKS = (1 << (SHIFT + 1));
+	const int BLOCKS_MASK = BLOCKS - 1;
+
+	if(log_size == 0 || end < begin + 2) return;
+	if(end - begin < 2000) { //picked arbitrarily
+		sort(begin, end, typename PathGraph<index_t>::PathNodeFromCmp());
+		return;
+	}
+
+	// compute maximum of log_size - 7 and 0
+	int right_shift = (log_size - SHIFT) * (log_size > SHIFT);
+	// count number in each bin
+	index_t count[BLOCKS] = {0};
+	for(T* curr = begin; curr != end; curr++) {
+		count[(hash(curr) >> right_shift) & BLOCKS_MASK]++;
+	}
+	// sum numbers to create an index
+	T* index[BLOCKS + 1];
+	T* place[BLOCKS];
+	index[0] = place[0] = begin;
+	for(int i = 1; i < BLOCKS; i++) {
+		index[i] = place[i] = index[i - 1] + count[i - 1];
+	}
+	index[BLOCKS] = end;
+	//put objects in proper place
+	for(int bin = 0; bin < BLOCKS; bin++) {
+		while(place[bin] != index[bin + 1]) {
+			T curr = *place[bin];
+			int x = (hash(&curr) >> right_shift) & BLOCKS_MASK;
+			while(x != bin) { // switched inner loop here, removed branch statement
+				T temp = *place[x];
+				*place[x]++ = curr;
+				curr = temp;
+				x = (hash(&curr) >> right_shift) & BLOCKS_MASK;
+			}
+			*place[bin]++ = curr;
+		}
+	}
+	//sort partitions
+	if(right_shift) {
+		for(int bin = 0; bin < BLOCKS; bin++)
+			if(index[bin + 1] - index[bin] > 1) bin_sort<T, index_t>(index[bin], index[bin + 1],hash, right_shift);
+	}
+}
+
+template <typename T, typename index_t>
+void bin_sort_copy(T* begin, T* end, T* o, index_t (*hash)(T*), index_t maxv) {
+	const int SHIFT = 7;
+	const int BLOCKS = (1 << (SHIFT + 1));
+
+	int log_size = sizeof(maxv) * 8;
+	while(!((1 << log_size) & maxv)) log_size--;
+	int right_shift = log_size - SHIFT;
+	int occupied = (maxv >> right_shift) + 1;
+	index_t count[BLOCKS] = {0};
+	// count number in each bin ~ 1/5 of time
+	for(T* curr = begin; curr != end; curr++) {
+		count[hash(curr) >> right_shift]++;
+	}
+	// sum numbers to create an index ~ takes very little time
+	T* index[BLOCKS];
+	index[0] = o;
+	for(int i = 1; i < occupied; i++) {
+		index[i] = index[i - 1] + count[i - 1];
+	}
+	// hash objects ~ 4/5 of time
+	for(T* curr = begin; curr != end; curr++) {
+		*index[hash(curr) >> right_shift]++ = *curr;
+	}
+	//sort partitions
+	bin_sort<T, index_t>(o, index[0], hash, right_shift);
+	for(int bin = 1; bin < occupied; bin++) {
+		bin_sort<T, index_t>(index[bin - 1], index[bin], hash, right_shift);
+	}
+}
+
+
 //Creates an initial PathGRaph from the RefGraph
 //All nodes begin as unsorted nodes
 template <typename index_t>
@@ -1839,10 +1913,10 @@ report_F_node_idx(0), report_F_location(0)
 
 		assert_neq(past_nodes.size(), ranks);
 
-		EList<PathNode> from_table; from_table.resizeExact(past_nodes.size()); from_table.fillZero();
+		EList<PathNode> from_table; from_table.resizeExact(past_nodes.size());// from_table.fillZero();
 		if(verbose) cerr << "ALLOCATE FROM_TABLE: " << (float)(clock() - indiv) / CLOCKS_PER_SEC << endl;
 		indiv = clock();
-		binSortByFrom(from_table.begin());
+		bin_sort_copy<PathNode, index_t>(past_nodes.begin(), past_nodes.end(), from_table.ptr(), &PathNodeFrom, max_from);
 		if(verbose) cerr << "BUILD TABLE: " << (float)(clock() - indiv) / CLOCKS_PER_SEC << endl;
 		indiv = clock();
 
@@ -2481,6 +2555,8 @@ void PathGraph<index_t>::binSortByFromWorker(PathNode* begin, PathNode* end, int
 			if(index[bin + 1] - index[bin] > 1) binSortByFromWorker(index[bin], index[bin + 1], right_shift);
 	}
 }
+
+
 
 template <typename index_t>
 void PathGraph<index_t>::binSortByTo(bool create_index) {
