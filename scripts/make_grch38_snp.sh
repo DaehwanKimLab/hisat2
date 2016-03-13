e10ece15556eb020341bc5e709e785d3848efa2b
Daehwan <dkim136@jhu.edu@login-node04.cm.cluster>
2016-03-13 10:05:14 -0400

    Changed make_grch38 scripts

--- scripts/make_grch38_snp.sh
+++ scripts/make_grch38_snp.sh
@@ -62,8 +62,7 @@ F=Homo_sapiens.GRCh38.dna.primary_assembly.fa
 if [ ! -f $F ] ; then
 	get ${ENSEMBL_GRCh38_BASE}/$F.gz || (echo "Error getting $F" && exit 1)
 	gunzip $F.gz || (echo "Error unzipping $F" && exit 1)
-	awk '{if($1 ~ /^>/) {print $1} else {print}}' $F > genome.fa
-	rm $F
+	mv $F genome.fa
 fi
 
 
