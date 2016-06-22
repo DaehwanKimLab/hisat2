e63da854836516d1307e9de8678f4ab8e9cd644c
Raymon Cao <rcao5@jhu.edu>
2016-06-22 14:53:36 -0400

    changed position to position + 1618

--- genotype_scripts/CYP2D6_MSF/debug_hisat2_makeMSF_CYP2D6.py
+++ genotype_scripts/CYP2D6_MSF/debug_hisat2_makeMSF_CYP2D6.py
@@ -61,8 +61,8 @@ def main():
 
             # @Daehwan - checking to make sure nt changes are compatible with seq (shows database errors in nt positions)
             try:
-                assert(cyp2d6_seq[pos - 1] == ntChange[0]) # nt at pos in seq must match database
+                assert(cyp2d6_seq[pos + 1618] == ntChange[0]) # nt at pos in seq must match database
             except:
-                print >> sys.stdout, "Warning: position %d in sequence contains %s, but expected %s from database" % (pos, cyp2d6_seq[pos - 1], ntChange[0])
+                print >> sys.stdout, "Warning: position %d in sequence contains %s, but expected %s from database" % (pos, cyp2d6_seq[pos + 1618], ntChange[0])
 
 main()
