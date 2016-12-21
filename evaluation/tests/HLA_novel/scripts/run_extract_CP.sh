9a4175d6add0ae455db1f74b9ffe264669cc8420
Daehwan Kim <infphilo@gmail.com>
2016-12-21 13:43:59 -0500

    .

--- evaluation/tests/HLA_novel/scripts/run_extract_CP.sh
+++ evaluation/tests/HLA_novel/scripts/run_extract_CP.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --job-name=infphio.HLA.CP.extract.genome
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=18
-#SBATCH --mem=120G
-#SBATCH --partition=shared
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=400G
+#SBATCH --partition=lrgmem
 #SBATCH --time=166:0:0 
 #SBATCH --workdir=/home-1/dkim136@jhu.edu/infphilo/hisat2/evaluation/tests/HLA_novel
 
-/home-1/dkim136@jhu.edu/infphilo/hisat2/evaluation/tests/HLA_novel/scripts/extract_reads.py --base-fname genotype_genome --reference-type genome --read-dir /home-1/dkim136@jhu.edu/aszalay1/genomes --out-dir CP_100 -p 18 --max-sample 100 --job-range 0,3
+/home-1/dkim136@jhu.edu/infphilo/hisat2/evaluation/tests/HLA_novel/scripts/extract_reads.py --base-fname genotype_genome --reference-type genome --read-dir /home-1/dkim136@jhu.edu/aszalay1/genomes --out-dir CP_80 -p 40 --max-sample 80 --job-range 0,2
 
