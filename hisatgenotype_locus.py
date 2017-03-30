1510e1184058ec5759e90c0fff7943e3a5bdcfca
infphilo <infphilo@gmail.com>
2017-03-30 07:39:42 -0400

    .

--- hisatgenotype_locus.py
+++ hisatgenotype_locus.py
@@ -2305,15 +2305,6 @@ def typing(ex_path,
                     print >> sys.stderr
                     print >> sys.stderr
 
-            # DK - debugging purposes
-            for allele_name, vars in allele_vars.items():
-                vars = set(vars)
-                # vars_ = set(["hv7453", "hv7454", "hv7456", "hv7457", "hv7458", "hv7460"])
-                vars_ = set(["hv7452", "hv7455", "hv7459", "hv7461"])
-                if vars >= vars_:
-                    print "DK:", allele_name
-            sys.exit(1)
-                
 
             # Identify alleles that perfectly or closesly match assembled alleles
             # """
@@ -2822,7 +2813,7 @@ def test_Gene_genotyping(base_fname,
         # test_list = [[["A*26:25N"]]] # for allele that includes an insertion
         # test_list = [[["A*24:36N"]]] # for normal allele?
         # test_list = [[["A*02:01:21"]], [["A*03:01:01:01"]], [["A*03:01:01:04"]], [["A*02:521"]]]
-        test_list = [[["B*15:01:01:04", "B*56:01:01:03"]]]
+        # test_list = [[["B*15:01:01:04", "B*56:01:01:03"]]]
         for test_i in range(len(test_list)):
             if "test_id" in daehwan_debug:
                 daehwan_test_ids = daehwan_debug["test_id"].split('-')
