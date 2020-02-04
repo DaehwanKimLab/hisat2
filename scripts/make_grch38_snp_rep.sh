d855dac135395fefc112e4afc5cf7365eb9f87d7
Chanhee Park <parkchanhee@gmail.com>
2020-02-04 16:31:58 -0600

    Add repeat index build scripts

---
+++ scripts/make_grch38_snp_rep.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+#
+# Downloads sequence for the GRCh38 release 84 version of H. sapiens (human) from
+# Ensembl.
+#
+# Note that Ensembl's GRCh38 build has three categories of compressed fasta
+# files:
+#
+# The base files, named ??.fa.gz
+#
+# By default, this script builds and index for just the base files,
+# since alignments to those sequences are the most useful.  To change
+# which categories are built by this script, edit the CHRS_TO_INDEX
+# variable below.
+#
+
+ENSEMBL_RELEASE=84
+ENSEMBL_GRCh38_BASE=ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/fasta/homo_sapiens/dna
+
+DBSNP_RELEASE=144
+SNP_FILE=snp${DBSNP_RELEASE}Common.txt
+UCSC_COMMON_SNP=http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/${SNP_FILE}
+
+get() {
+	file=$1
+	if ! wget --version >/dev/null 2>/dev/null ; then
+		if ! curl --version >/dev/null 2>/dev/null ; then
+			echo "Please install wget or curl somewhere in your PATH"
+			exit 1
+		fi
+		curl -o `basename $1` $1
+		return $?
+	else
+		wget $1
+		return $?
+	fi
+}
+
+HISAT2_BUILD_EXE=./hisat2-build
+if [ ! -x "$HISAT2_BUILD_EXE" ] ; then
+	if ! which hisat2-build ; then
+		echo "Could not find hisat2-build in current directory or in PATH"
+		exit 1
+	else
+		HISAT2_BUILD_EXE=`which hisat2-build`
+	fi
+fi
+
+HISAT2_SNP_SCRIPT=./hisat2_extract_snps_haplotypes_UCSC.py
+if [ ! -x "$HISAT2_SNP_SCRIPT" ] ; then
+	if ! which hisat2_extract_snps_haplotypes_UCSC.py ; then
+		echo "Could not find hisat2_extract_snps_haplotypes_UCSC.py in current directory or in PATH"
+		exit 1
+	else
+		HISAT2_SNP_SCRIPT=`which hisat2_extract_snps_haplotypes_UCSC.py`
+	fi
+fi
+
+HISAT2_REPEAT_EXE=./hisat2-repeat
+if [ ! -x "$HISAT2_REPEAT_EXE" ]; then
+	if ! which hisat2-repeat ; then
+		echo "Could not find hisat2-repeat in current directory or in PATH"
+		exit 1
+	else
+		HISAT2_REPEAT_EXE=`which hisat2-repeat`
+	fi
+fi
+
+rm -f genome.fa
+F=Homo_sapiens.GRCh38.dna.primary_assembly.fa
+if [ ! -f $F ] ; then
+	get ${ENSEMBL_GRCh38_BASE}/$F.gz || (echo "Error getting $F" && exit 1)
+	gunzip $F.gz || (echo "Error unzipping $F" && exit 1)
+	mv $F genome.fa
+fi
+
+
+if [ ! -f $SNP_FILE ] ; then
+       get ${UCSC_COMMON_SNP}.gz || (echo "Error getting ${UCSC_COMMON_SNP}" && exit 1)
+       gunzip ${SNP_FILE}.gz || (echo "Error unzipping ${SNP_FILE}" && exit 1)
+       awk 'BEGIN{OFS="\t"} {if($2 ~ /^chr/) {$2 = substr($2, 4)}; if($2 == "M") {$2 = "MT"} print}' ${SNP_FILE} > ${SNP_FILE}.tmp
+       mv ${SNP_FILE}.tmp ${SNP_FILE}
+       ${HISAT2_SNP_SCRIPT} genome.fa ${SNP_FILE} genome
+fi
+
+# Build repeat
+CMD="${HISAT2_REPEAT_EXE} -p 4 --repeat-count 5 --repeat-length 51-300,76-300,100-300,101-300,151-300 genome.fa genome_rep"
+echo Running $CMD
+$CMD 
+[[ "$?" -eq 0 ]] || { echo "Index building failed; see error message";  exit 1; };
+
+CMD="${HISAT2_BUILD_EXE} -p 4 genome.fa --snp genome.snp --haplotype genome.haplotype --repeat-ref genome_rep.rep.fa --repeat-info genome_rep.rep.info genome_snp_rep"
+echo Running $CMD
+if $CMD ; then
+	echo "genome index built; you may remove fasta files"
+else
+	echo "Index building failed; see error message"
+fi
