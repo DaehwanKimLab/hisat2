24363bb8ed8e519d649aa0b6c384534ba3f838dc
Chanhee Park <parkchanhee@gmail.com>
2020-12-11 16:56:55 -0600

    .

--- evaluation/simulation/calculate_read_cost.py
+++ evaluation/simulation/calculate_read_cost.py
@@ -1993,7 +1993,7 @@ def calculate_read_cost(single_end,
         #["hisat2", "", "tran", "221", ""],  # original version
         ["hisat2", "", "tran", "", ""],  # original version
         #["hisat-gt", "", "", "", ""],
-        ["hisat-gt", "", "snp", "", ""],
+        ["hisat-gt", "", "", "", ""],
         # ["hisat2", "", "rep-100-300", "", ""],
         # ["hisat2", "", "rep_mm", "", ""],
         # ["hisat2", "", "", "", "--sensitive"],
