17e5018b5ad61723b46ade4e3b8a609a160d56f0
Daehwan Kim <infphilo@gmail.com>
2020-12-24 09:58:25 -0600

    Made build_indexes.py to work with Python3, and added several options such as aligner-list, genome-list, and fresh

--- evaluation/build_indexes.py
+++ evaluation/build_indexes.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys, os
-use_message = '''
-'''
+from argparse import ArgumentParser, FileType
 
 # GRCh38 release 84
-def build_indexes():    
+def build_indexes(input_aligners,
+                  input_genomes,
+                  fresh):
     # Build indexes
     if not os.path.exists("indexes"):
         os.mkdir("indexes")
     os.chdir("indexes")
-    aligners = ["HISAT2", "HISAT-GT", "HISAT", "Bowtie", "STAR", "GSNAP", "BWA", "minimap2", "salmon", "kallisto"]
-    genomes = ["22_20-21M", "22", "genome"]
+    
+    if input_aligners:
+        aligners = input_aligners
+    else:
+        aligners = ["hisat2", "hisat-gt", "hisat", "bowtie", "star", "gsnap", "bwa", "minimap2", "salmon", "kallisto"]
+        
+    if input_genomes:
+        genomes = input_genomes
+    else:
+        genomes = ["22_20-21M", "22", "genome"]
+
     for genome in genomes:
         for aligner in aligners:
             if genome == "genome":
@@ -19,34 +29,37 @@ def build_indexes():
             else:
                 dir = aligner + "_" + genome
             if os.path.exists(dir):
-                continue
+                if fresh:
+                    os.system("rm -rf %s" % (dir))
+                else:
+                    continue
             os.mkdir(dir)
             os.chdir(dir)
-            if aligner == "HISAT2":
+            if aligner == "hisat2":
                 cmd = "../../aligners/bin/hisat2-build ../../data/%s.fa %s" % (genome, genome)
                 cmd = cmd + "; ../../aligners/bin/hisat2-build -p 4 ../../data/%s.fa --snp ../../data/%s.snp --haplotype ../../data/%s.haplotype %s_snp" % (genome, genome, genome, genome)
                 cmd = cmd + "; ../../aligners/bin/hisat2-build -p 4 ../../data/%s.fa --ss ../../data/%s.ss --exon ../../data/%s.exon %s_tran" % (genome, genome, genome, genome)
                 cmd = cmd + "; ../../aligners/bin/hisat2-build -p 4 ../../data/%s.fa --snp ../../data/%s.snp --haplotype ../../data/%s.haplotype --ss ../../data/%s.ss --exon ../../data/%s.exon %s_snp_tran" % (genome, genome, genome, genome, genome, genome)
-            elif aligner == "HISAT-GT":
+            elif aligner == "hisat-gt":
                 cmd = "../../aligners/bin/hisat2_extract_transcript_graph.py -g ../../data/%s.gtf -r ../../data/%s.fa --chrtome -o ../../data/%s" % (genome, genome, genome)
                 cmd = cmd + ";../../aligners/bin/hisat2-build -p 4 ../../data/%s.gt.fa --ss ../../data/%s.gt.ss %s.gt" % (genome, genome, genome)
-            elif aligner == "HISAT":
+            elif aligner == "hisat":
                 cmd = "../../aligners/bin/hisat-build ../../data/%s.fa %s" % (genome, genome)
                 cmd = cmd + "; ../../aligners/bin/tophat -G ../../data/%s.gtf --transcriptome-index=gtf %s; rm -rf tophat_out" % (genome, genome)
-            elif aligner == "Bowtie":
+            elif aligner == "bowtie":
                 cmd = "../../aligners/bin/bowtie-build ../../data/%s.fa %s" % (genome, genome)
-            elif aligner == "Bowtie2":
+            elif aligner == "bowtie2":
                 cmd = "../../aligners/bin/bowtie2-build --threads 6 ../../data/%s.fa %s" % (genome, genome)
-            elif aligner == "STAR":
+            elif aligner == "star":
                 cmd = "../../aligners/bin/STAR --runMode genomeGenerate --genomeDir . --genomeFastaFiles ../../data/%s.fa" % (genome)
                 cmd = cmd + "; mkdir gtf; ../../aligners/bin/STAR --runMode genomeGenerate --genomeDir gtf --genomeFastaFiles ../../data/%s.fa --sjdbGTFfile ../../data/%s.gtf --sjdbOverhang 99 --runThreadN 4" % (genome, genome)
-            elif aligner == "GSNAP":
+            elif aligner == "gsnap":
                 cmd = "../../aligners/bin/gmap_build -B ../../aligners/bin -D . -d %s ../../data/%s.fa" % (genome, genome)
-            elif aligner == "BWA":
+            elif aligner == "bwa":
                 cmd = "../../aligners/bin/bwa index -p %s.fa ../../data/%s.fa" % (genome, genome)
             elif aligner == "minimap2":
                 cmd = "../../aligners/bin/minimap2 -x sr -d %s.mmi ../../data/%s.fa" % (genome, genome)
-            elif aligner == "VG":
+            elif aligner == "vg":
                 assert False
             elif aligner == 'salmon':
                 cmd = ""
@@ -64,7 +77,7 @@ def build_indexes():
                     cmd += ' && ../../aligners/bin/kallisto index -i genome ../../data/genome.cdna.fa'
             else:
                 assert False
-            print >> sys.stderr, cmd
+            print(cmd, file=sys.stderr)
             os.system(cmd)
             os.chdir("..")
 
@@ -72,4 +85,42 @@ def build_indexes():
             
     
 if __name__ == "__main__":
-    build_indexes()
+    parser = ArgumentParser(
+        description='Build indexes for HISAT2, HISAT-GT, STAR, Bowtie1/2, GSNAP, BWA-mem, etc.')
+    parser.add_argument('--aligner-list',
+                        dest='aligner_list',
+                        type=str,
+                        default="",
+                        help='comma-separated list of aligners (e.g. hisat2,hisat-gt,bowtie2,bwa')
+    parser.add_argument('--genome-list',
+                        dest='genome_list',
+                        type=str,
+                        default="",
+                        help='comma-separated list of genomes (e.g. 22,genome')
+    parser.add_argument('--fresh',
+                        dest='fresh',
+                        action='store_true',
+                        help='delete existing alignment related directories (e.g. hisat2_single)')
+    parser.add_argument('-v', '--verbose',
+                        dest='verbose',
+                        action='store_true',
+                        help='also print some statistics to stderr')
+
+    args = parser.parse_args()
+
+    aligners = []
+    for aligner in args.aligner_list.split(','):
+        if aligner == "":
+            continue
+        aligners.append(aligner)
+
+    genomes = []
+    for genome in args.genome_list.split(','):
+        if genome == "":
+            continue
+        genomes.append(genome)
+
+    build_indexes(aligners,
+                  genomes,
+                  args.fresh)
+    
