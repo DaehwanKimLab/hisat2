8caa770f49964dfc173016d5e3cc309fdb5d8707
mathornton01 <mathornton@smu.edu>
2021-10-27 11:52:10 -0500

    Got Simple Read Generator working

--- simulator-expanded/genFasta.sh
+++ simulator-expanded/genFasta.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 function usage {
-	echo "./$(basename $0) -h --> Shows Usage"
+	echo "Quickly Generate a set of Simulated Reads from a FASTA Assembly file." 
+	echo "Usage: ./genFasta.sh -g [Genome (.fasta) File Name] -n [Number of Reads to Generate]" 
+        echo "                     -s [Length of Reads to Generate] -o [Output File Name]         "
+	echo "                     -v <Run in verbose mode> -t <Use true random values WIP>       "
+        echo "                     -f <Filter out lines with only N at top of genome file>        "
+        echo "                     -h <Show this Help Message>	                                  "
+	echo "------------------------------------------------------------------------------------"
+	echo "Version 1.0                                                                         "
 }
 verbose=false;
 truerandom=false;
-while getopts "g:n:s:o:vth" flag
+filter=false;
+while getopts "g:n:s:o:vthf" flag
 do
 	case "${flag}" in
 		g) genfile=${OPTARG};;
@@ -13,6 +21,7 @@ do
 		o) outfile=${OPTARG};;
 		v) verbose=true;;
 		t) truerandom=true;;
+		f) filter=true;;
 		h) usage
 		   exit;;
 	esac
@@ -24,6 +33,26 @@ echo "Number of Reads: $numread";
 echo "Read Length: $readlen"; 
 echo "Output File: $outfile";  
 fi;
+
+if [[ -e $outfile ]]; then 
+	echo "Warning:  The file $outfile already exists"
+	echo "remove it? (y) (n)";
+	read -p '[y/n]: ' choice;
+	while [[ ! $choice =~ ^[yn]$ ]]; 
+	do
+		read -p '[y/n]: ' choice;
+	done
+if [[ $choice =~ ^y$ ]]; then 
+	echo "Removing $outfile ...";	
+	rm -f $outfile;
+else 
+	echo "Please specify alternative file."; 
+	exit
+fi
+
+
+fi
+
 if [[ ! -s $genfile ]]; then 
        echo "Error: File $genfile does not exist exiting"; 
        exit;
@@ -44,3 +73,30 @@ if $verbose; then
 echo "Genome File Length: $genomeFileLen Effective Number of Locations: $locsForReading";
 fi;
 
+beggenfile=1;
+charperline=$(($(head -n 2 $genfile | grep NNNNNNN$ | wc -c)));
+linesN=$(($(cat $genfile | grep NNNNNNN$ | wc -l)));
+
+if [[ $filter ]]; then 
+	beggenfile=$(( $charperline * $linesN ));
+fi
+
+
+shuf -i $beggenfile-$(($genomeFileLen - $readlen)) -n $((numread)) > temp_locs.txt
+STARTS=$(cat temp_locs.txt)
+
+scaf=$(head $genfile |grep ">" | tr -d ">")
+for START in $STARTS
+do
+	echo "$scaf	$START	$(($START+$readlen))" >> temp_locs_ranges.bed;
+done;
+
+
+if $verbose; then 
+	echo "List of Locations Generated: locations_temp.txt";
+fi
+
+
+ bedtools getfasta -fi $genfile -bed temp_locs_ranges.bed > $outfile
+ rm temp_locs.txt
+ rm temp_locs_ranges.bed
